8c0ccfcf75a3b6f14f9d8dd20d47ecbd4f9f45fa
Hafiz Mustafa <mhd.hfzz@gmail.com>
2021-12-01 09:47:28 +0800

    add total expenses and style

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using PocketBalance.Data;
+using PocketBalance.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +13,32 @@ namespace PocketBalance.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PocketBalanceContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, PocketBalanceContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
+        [BindProperty]
+        public Expense Expense { get; set; }
+        public float TotalExpenses { get; set; }
+        public float TotalExpensesByMonth { get; set; }
+        public float TotalExpensesByWeek { get; set; }
+
+
+
+
+
 
         public void OnGet()
         {
-
+           TotalExpenses = _context.Expense.Select(e => e.Amount).Sum();
+           TotalExpensesByMonth = _context.Expense.Where(d => d.ExpenseDate > DateTime.Now.AddMonths(-1))
+                                                   .Select(e => e.Amount).Sum();
+            TotalExpensesByWeek = _context.Expense.Where(d => d.ExpenseDate > DateTime.Now.AddDays(-7))
+                                                   .Select(e => e.Amount).Sum();
         }
     }
 }
